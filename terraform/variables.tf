0b5edf85f98b7fd9afb63a0ea847e1246faba35b
Vibin Kishore <vkishore@192.168.1.3>
2023-10-17 20:43:46 +0530

    final changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
 variable "confluent_cloud_api_key" {
   description = "Confluent Cloud API Key (also referred as Cloud API ID)"
   type        = string
-  default = "WJZ2YA323XHB5DWQ"
+  default = "<cloud_api_key>"
 }
 
 variable "confluent_cloud_api_secret" {
   description = "Confluent Cloud API Secret"
   type        = string
   sensitive   = true
-  default = "kZ7miPvZ41sop/wcWOwKT5mlQ65mGeQO6xTYCh+gvzKfYuUdoUrJYnjwPhFVLIcH"
+  default = "<cloud_api_secret>"
 }
